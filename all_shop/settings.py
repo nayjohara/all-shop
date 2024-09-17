8a7d3a029a9a067127fbf7edfccf1b33644acb42
nayjohara <nayajoharar@gmail.com>
2024-09-17 17:36:35 +0700

    menambahkan form, xml, json

--- all_shop/settings.py
+++ all_shop/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://najya-johara-allshop.pbp.cs.ui.ac.id/", "https://najya-johara-allshop.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://najya-johara-allshop.pbp.cs.ui.ac.id/", "https://http://najya-johara-allshop.pbp.cs.ui.ac.id/"]
